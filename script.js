0679127c8a0f0a220b7f5ba2525a91aa99aed6c2
fanny <fannyvolc@gmail.com>
2022-06-16 02:32:03 +0200

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+// Created by Emms 
+
+alert(`An idea by: Techno Haasaan, a weird, amazing and funny friend here on Sololearn.
+
+Visit him here: https://www.sololearn.com/Profile/21800447/?ref=app
+
+Minus Html, Comments, extral spaces and few other styles, this code is more than 1000 lines of CSS codes, first time I reached such milestone.  
+
+It also took a month and some no sleeping today challenge to complete. So please Like & Comment.`);
